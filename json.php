910c7b2af4f628f05b2476e5b2d771e3df9c3e8f
Hiliston <50718354+Hiliston@users.noreply.github.com>
2019-10-25 18:22:20 +0300

    Add files via upload

--- json.php
+++ json.php
@@ -2,43 +2,49 @@
 function getJSONAsk2($array)
 {
     $json_ask = json_decode($array, true);
+    if(!empty($json_ask)){
+        $auth = globalAuth($json_ask);
+        //var_dump($auth);
 
-    $auth = globalAuth($json_ask);
-    //var_dump($auth);
+        $command = $json_ask['command'];
+        
+        if($auth['success'] == "true"){
+            $status = "true";
+            $data = makeData($json_ask);
+            $array_answer = [                                     //Массив для ответа оп API
+                "status" => "$status",
+                "command" => "$command",
+                "auth" => $auth,                     //$auth - Массив с блоком авторизации
+                "data" => $data
+            ];
+        }else{
+            $status = "false";
+            $data = "none";
+            $array_answer = [                                     //Массив для ответа оп API
+                "status" => "$status",
+                "command" => "$command",
+                "auth" => $auth,                     //$auth - Массив с блоком авторизации
+                "data" => $data
+            ];
+        }
+
+        $json_answer = json_encode($array_answer);
+        return $json_answer;
 
 
-    $command = $json_ask['command'];
 
 
-
-
-
-
-
-    if($auth['success'] == "true"){
-        $status = "true";
-        $data = makeData($json_ask);
-        $array_answer = [                                     //Массив для ответа оп API
-            "status" => "$status",
-            "command" => "$command",
-            "auth" => $auth,                     //$auth - Массив с блоком авторизации
-            "data" => $data
-        ];
     }else{
         $status = "false";
         $data = "none";
         $array_answer = [                                     //Массив для ответа оп API
             "status" => "$status",
-            "command" => "$command",
-            "auth" => $auth,                     //$auth - Массив с блоком авторизации
+            "command" => "none",
+            "auth" => "none",                     //$auth - Массив с блоком авторизации
             "data" => $data
         ];
+        $json_answer = json_encode($array_answer);
+        return $json_answer;
     }
-
-    $json_answer = json_encode($array_answer);
-    return $json_answer;
-}
-
-function getJSONAnswer2(){
 
 }
